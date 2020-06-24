4895d192dbdcee98fca2eee62500d8a8f38b3b2e
Martin Carlsson <martin.carlsson@stenaline.com>
2020-06-24 20:19:44 +0200

    A bit more up to date.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,79 +1,180 @@
-;; Useful constant
-(defconst console-p (eq (symbol-value 'window-system) nil))
+;; I never use the toolbar and menubar and I prefer a darker theme.
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
 
-;; UI modifications and
-;; a nice theame from Enrique
-(show-paren-mode t)
-(when (not console-p)
-  (tool-bar-mode 0)
-  (scroll-bar-mode -1)
-  (setq frame-title-format "emacs - %b")
-  (setq inhibit-startup-message t)
-  (add-to-list 'default-frame-alist '(font . "Menlo-12"))
-  (setq font-lock-face-attributes
-        '((font-lock-comment-face       "#B0B0B0")
-;          (font-lock-string-face        "#EC4600")
-          (font-lock-string-face        "#B03060")
-          (font-lock-keyword-face       "#7045A1")
-          (font-lock-constant-face      "#277C8E")
-          (font-lock-builtin-face       "Black")
-          (font-lock-function-name-face "Black")
-          (font-lock-variable-name-face "#30448B")
-          (font-lock-type-face          "#3A5799")
-          (font-lock-warning-face       "Orchid"))))
+(global-display-line-numbers-mode 1)
+(setq display-line-numbers 'relative)
 
+(setq
+ inhibit-startup-screen t
+ inhibit-startup-message t
+ use-dialog-box nil
+ echo-keystrokes 0.02
+ resize-mini-windows 'grow-only
+ max-mini-window-height 0.15
+ make-backup-files nil)
 
-;; Console mode
-(when console-p
-  (setq inhibit-startup-message t))
+;; Why is ~/.emacs.d/site-lisp not added to the default load-path
+(add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
 
-;; Setup global load-paths
-(add-to-list 'exec-path "/Users/martin/Library/UNIX/bin")
-(add-to-list 'exec-path "/usr/local/bin")
-(add-to-list 'load-path "/Users/martin/.emacs.d/")
-(add-to-list 'load-path "/Users/martin/.emacs.d/site-lisp")
-(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/")
+;; Load the stright package management system which allows us
+;; to manage and define packages and versions in the init file
+;; without integrating without 'package.el'
+(load (expand-file-name "straight.el" user-emacs-directory))
 
-;; Set basic information
-(setq user-mail-address "spearalot@gmail.com")
-(setq user-full-name "Martin Carlson")
+;; Setup use-package
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
-;; Editing tweeks
-(set-language-environment "UTF-8")
-(setq backup-directory-alist '((".*" . "~/.emacs.d/backups")))
-(setq-default indent-tabs-mode nil)
-(setq undo-limit 20971520)
-(setq undo-strong-limit 22020096)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; DOOM themes
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-one t))
 
-;; OS X specific
-(setq ns-alternate-modifier 'nil)
-(setq ns-command-modifier 'meta)
+;; Parenthesis
+(use-package paredit
+  :config (enable-paredit-mode))
 
-;; Spelling
-(setq-default ispell-program-name "aspell")
+;; Highlight matching parens
+(show-paren-mode)
+(setq show-paren-delay 0.1
+      show-paren-highlight-openparen t
+      show-paren-when-point-inside-paren t
+      show-paren-when-point-in-periphery t)
 
-;; Abbrrev
-(setq abbrev-file-name "~/.emacs.d/abbrev_defs")
-(setq save-abbrevs t)
-(quietly-read-abbrev-file)
+;; Whitespaces
+(setq whitespace-line-column nil
+      whitespace-style
+      '(face indentation tabs tab-mark spaces space-mark newline newline-mark
+	     trailing lines-tail)
+      whitespace-display-mappings
+      '((tab-mark ?\t [?› ?\t])
+	(newline-mark ?\n [?¬ ?\n])
+	(space-mark ?\  [?·] [?.])))
 
-;; Erang mode
-(load "start-erlang.el")
+;; Hydra
+(use-package hydra
+  :load-path "site-lisp/hydra")
 
-;; OCAML
-(load "start-ocaml.el")
+;; Projectile
+(use-package projectile
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :hook (after-init . projectile-mode))
 
-;; SLime
-(load "start-slime.el")
+;; Use ripgrep
+(use-package projectile-ripgrep)
 
-;; Scheme
-(load "start-scheme.el")
+;; YAS
+(use-package yasnippet
+  :hook (after-init . yas-global-mode))
+(use-package auto-yasnippet)
 
-;; W3N
-(add-to-list 'load-path "/Users/martin/.emacs.d/emacs-w3m")
-(setq browse-url-browser-function 'w3m-browse-url)
-(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-(require 'w3m-ems)
+;; Company
+(use-package company
+  :init (setq company-idle-delay 0.25
+	      company-minimum-prefix-length 2
+	      company-tooltip-limit 14
+	      company-tooltip-align-annotations t
+	      company-require-match 'never
+	      company-dabbrev-ignore-case nil
+	      company-dabbrev-ignore-case nil
+	      company-dabbrev-downcase nil)
+  :hook (after-init . global-company-mode)
+  :bind (:map company-active-map
+	      ("C-p" . 'company-select-previous)
+	      ("C-n" . 'company-select-next)))
 
-;; Emacs generated stuff...
+;(use-package company-dict)
+;(use-package company-prescient)
+
+;; Helm
+(use-package helm
+  :init (setq helm-display-header-line nil
+	      helm-mode-line-string nil
+	      helm-ff-auto-update-initial-value nil
+	      helm-find-files-doc-header nil
+	      helm-display-buffer-default-width nil
+	      helm-display-buffer-default-height 0.25
+	      helm-imenu-execute-action-at-once-if-one nil
+	      helm-ff-lynx-style-map nil
+	      helm-M-x-fuzzy-match t
+	      helm-buffers-fuzzy-matching t
+	      helm-recentf-fuzzy-match t)
+  :bind (("M-x"   . 'helm-M-x)
+	 ("M-y"   . 'helm-show-kill-ring)
+	 ("C-c h" . 'helm-mini)
+	 ([remap find-file] . 'helm-find-files)
+	 (:map helm-map
+	       ("C-p" . 'helm-previous-line)
+	       ("C-n" . 'helm-next-line))))
+
+;; Use ripgrep
+(use-package helm-rg)
+
+;; Helm for projectile
+(use-package helm-projectile
+  :hook (after-init . helm-projectile-on)
+  :after (helm))
+
+;; Company integration
+(use-package helm-company
+  :after (helm))
+
+;; Helm swoop
+(use-package helm-swoop
+  :after (helm)
+  :bind (:map helm-swoop-map
+	      ("C-p" . 'helm-previous-line)
+	      ("C-n" . 'helm-nex-line)))
+
+;; MAGIT!
+(use-package magit)
+
+;;
+;; Below is some more prettyfications
+;;
+(use-package all-the-icons
+  :commands (all-the-icons-octicon
+             all-the-icons-faicon
+             all-the-icons-fileicon
+             all-the-icons-wicon
+             all-the-icons-material
+             all-the-icons-alltheicon))
+
+(use-package rainbow-delimiters
+  :init (setq rainbow-delimiters-max-face-count 3)
+  :hook (after-init . rainbow-delimiters-mode))
+
+;; A pretty mode-line
+;(use-package anzu
+;  :config
+;  (global-anzu-mode t))
+  ;:config
+  ;(progn
+  ;  (global-anzu-mode +1)
+  ;  (global-set-key [remap query-replace] 'anzu-query-replace)
+  ;  (global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp))
+  
+;; "My own" normal mode
+(require 'modal)
+(global-set-key (kbd "<f1>")
+		(lambda ()
+		  (interactive)
+		  (keyboard-escape-quit)
+		  (modal-mode 1)))
+
+;; LSP
+(use-package eglot
+  :bind (:map eglot-mode-map
+	      ("d" . xref-find-definitions)
+              ("h" . eglot-help-at-point)
+              ("a" . eglot-code-actions)
+	      ("r" . eglot-rename))
+  :bind-keymap ("C-c l" . eglot-mode-map))
+
+;; Python mode
+(use-package python
+  :mode ("\\.py\\'" . python-mode))
--- emacs.d/site-lisp/start-scheme.el
+++
@@ -1,6 +0,0 @@
-; Scheme 48
-;(require 'cmuscheme48)
-;(setq scheme-program-name "scheme48")
-
-(load "/Users/martin/Library/UNIX/lib/gambit/share/emacs/site-lisp/gambit.el")
-(require 'gambit)
