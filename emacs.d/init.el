1288cfff94d63f9b1f396b7844fe3e951079b236
Martin Carlsson <martin.carlsson@stenaline.com>
2021-02-26 12:56:40 +0100

    Removed bad pkgs, fixed lsp.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -20,7 +20,6 @@
 (add-to-list 'load-path (expand-file-name "site-lisp" user-emacs-directory))
 (load "~/.emacs.d/private.el")
 
-
 ;; Load the stright package management system which allows us
 ;; to manage and define packages and versions in the init file
 ;; without integrating without 'package.el'
@@ -43,7 +42,9 @@
 
 ;; A proper editor
 (use-package evil
-  :init (setq evil-want-keybinding nil)
+  :init
+  (setq evil-want-keybinding nil
+        evil-move-beyond-eol t)
   :config (evil-mode 1))
 (use-package evil-collection
   :after evil)
@@ -57,62 +58,17 @@
   :config (global-evil-surround-mode 1))
 (use-package evil-commentary
   :after evil
-  :config (evil-commentary-mode))
-(use-package evil-cleverparens
-  :after evil
-  :hook (emacs-lisp . evil-cleverparens-mode))
-
-;; Keybindings
-(use-package general
-  :config
-  (general-create-definer leader-defs
-    :states '(normal insert visual emacs)
-    :prefix "SPC"
-    :non-normal-prefix "M-SPC")
-
-  ;; Help
-  (general-create-definer leader-defs-help :wrapping leader-defs :infix "h")
-   (leader-defs-help
-   "v" 'describe-variable
-   "k" 'describe-key)
-
-  ;; Project
-  (general-create-definer leader-defs-project :wrapping leader-defs :infix "p")
-  (leader-defs-project
-    "p" 'projectile-switch-project
-    "f" 'projectile-find-file
-    "r" 'projectile-ripgrep)
-
-  ;; VC
-  (general-create-definer leader-defs-vc :wrapping leader-defs :infix "g")
-  (leader-defs-vc
-    "g" 'magit)
-
-  ;; Code
-  (general-create-definer leader-defs-code :wrapping leader-defs :infix "c")
-  (leader-defs-code
-   "c" 'comment-or-uncomment-region
-   "d" 'lsp-find-definition
-   "r" 'lsp-find-references))
+  :hook (prog-mode . evil-commentary-mode))
 
 ;; Fuzz dat shit
 (use-package ivy
+  :after (projectile company evil)
   :config (ivy-mode 1))
-;; (use-package counsel
-;;   :after ivy
-;;   :config
-;;   (counsel-mode 1)
-;;   (counsel-projectile t))
-
-;; Highlight matching parens
-(show-paren-mode)
-(setq show-paren-delay 0.1
-      show-paren-highlight-openparen t
-      show-paren-when-point-inside-paren t
-      show-paren-when-point-in-periphery t)
-
-;; Whitespaces
-(use-package highlight-chars)
+(use-package counsel
+  :after ivy)
+(use-package ivy-prescient
+  :after counsel
+  :config (ivy-prescient-mode 1))
 
 ;; Projectile
 (use-package projectile
@@ -120,11 +76,6 @@
 
 ;; Use ripgrep
 (use-package projectile-ripgrep)
-
-;; YAS
-(use-package yasnippet
-  :hook (after-init . yas-global-mode))
-(use-package auto-yasnippet)
 
 ;; Company
 (use-package company
@@ -138,11 +89,64 @@
         company-dabbrev-ignore-case nil
         company-dabbrev-downcase nil
         company-global-modes '(not eshell-mode))
-  :config
-  (company-mode 1))
-
+  :hook (prog-mode . company-mode)
+  :bind ((:map company-mode-map
+               ("C-p" . company-prev)
+               ("C-n" . company-next))))
+(use-package company-prescient
+  :after company
+  :config (company-prescient-mode 1))
 ;; (use-package company-dict)
-;; (use-package company-prescient)
+
+;; Keybindings
+(use-package general
+  :config
+  (general-create-definer leader-defs
+    :states '(normal insert visual emacs)
+    :prefix "SPC"
+    :non-normal-prefix "M-SPC")
+
+  ;; Help
+  (general-create-definer leader-defs-help :wrapping leader-defs :infix "h")
+  (leader-defs-help
+    "v" 'counsel-describe-variable
+    "f" 'counsel-describe-function
+    "k" 'describe-key)
+
+  ;; Files
+  (general-create-definer leader-defs-vc :wrapping leader-defs :infix "f")
+  (leader-defs-vc
+    "f" 'counsel-find-file
+    "d" 'counsel-dired
+    "z" 'counsel-fzf)
+
+  ;; VC
+  (general-create-definer leader-defs-vc :wrapping leader-defs :infix "g")
+  (leader-defs-vc
+    "g" 'magit
+    "s" 'magit-stage)
+
+  ;; Project
+  (general-create-definer leader-defs-project :wrapping leader-defs :infix "p")
+  (leader-defs-project
+    "p" 'projectile-switch-project
+    "f" 'projectile-find-file
+    "r" 'projectile-ripgrep)
+
+  ;; Code
+  (general-create-definer leader-defs-code :wrapping leader-defs :infix "c")
+  (leader-defs-code
+   "c" 'comment-or-uncomment-region
+   "d" 'lsp-find-definition
+   "r" 'lsp-find-references
+   "i" 'evil-indent-line)
+
+  ;; Completes
+  (general-create-definer leader-defs-completes :wrapping leader-defs :infix "x")
+  (leader-defs-completes
+    "c" 'company-complete
+    "f" 'company-files
+    "y" 'company-yassnippet))
 
 ;; Flycheck
 (use-package flycheck
@@ -150,6 +154,12 @@
 
 ;; MAGIT!
 (use-package magit)
+
+;; YAS
+;; (use-package yasnippet
+;;   :hook (prog-mode . yas-mode)
+;;   :config
+;;   (yas-load-directory (expand-file-name "yas" user-emacs-directory)))
 
 ;; Below are some more prettyfications
 (use-package all-the-icons
@@ -166,18 +176,48 @@
   (setq rainbow-delimiters-max-face-count 3)
   :hook (prog-mode . rainbow-delimiters-mode))
 
+;; Markdown
+(use-package markdown-mode)
+
+;; YAML
+(use-package yaml-mode)
+
+;; Erlang
+(use-package erlang)
+
+;; Typescript
+(use-package typescript-mode
+  :config
+  (setq typescript-indent-level 2)
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-mode))
+  :hook (typescript-mode . (lambda ()
+                             (add-to-list 'projectile-project-root-files "package.json")
+                             (add-to-list 'projectile-ignored-directories "node_modules"))))
+
 ;; Language server
 (use-package lsp-mode
-  :hook (python-mode . lsp)
+  :hook
+  ((python-mode . lsp)
+   (typescript-mode . lsp))
   :config
-  (setq lsp-lens-enable nil
+  (setq lsp-keymap-prefix nil
+        lsp-lens-enable nil
         lsp-headerline-breadcrumb-enable nil
-        lsp-modeline-code-actions-enable nil)
+        lsp-modeline-code-actions-enable nil
+        lsp-enable-folding nil
+        lsp-enable-text-document-color nil
+        lsp-enable-on-type-formatting nil)
+  ;; Set reasonable deps for flatpak docker
+  (progn
+    (require 'lsp-pyls)
+    (require 'lsp-javascript)
+    (lsp-dependency 'python (list :system "/usr/bin/flatpak-spawn"))
+    (lsp-dependency 'typescript (list :system "/usr/bin/flatpak-spawn")))
   :commands lsp)
 
 ;; Keep the junk away
 (use-package lsp-docker
-  ;; :straight (lsp-docker :type git :host github :repo "spearalot/lsp-docker")
+  :straight (lsp-docker :type git :host github :repo "spearalot/lsp-docker")
   :after lsp-mode
   :config
   (setq lsp-docker-command "flatpak-spawn --host docker")
@@ -194,13 +234,3 @@
 (use-package company-lsp
   :after lsp-mode
   :commands company-lsp)
-
-;; Markdown
-(use-package markdown-mode)
-
-;; Typescript
-(use-package typescript-mode)
-
-;; YAML
-(use-package yaml-mode
-  :hook yaml 'indent-tools-minor-mode)
